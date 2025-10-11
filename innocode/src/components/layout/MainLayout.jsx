f06be3d59015ba6ea3fa87f397f510e81e5d02f7
PeterTLoc <locnctse182454@fpt.edu.vn>
2025-10-11 09:55:21 +0700

    Merge thaitai to master in branch loc

--- innocode/src/components/layout/MainLayout.jsx
+++ innocode/src/components/layout/MainLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../navbar/Navbar';
-import Sidebar from '../sidebar/sidebar';
+import Sidebar from '../sidebar/Sidebar';
 
 export default function MainLayout() {
   return (
