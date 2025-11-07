1a24a3de0ac7bf1db185746ec6a711a230d440ca
PeterTLoc <locnctse182454@fpt.edu.vn>
2025-11-07 16:12:18 +0700

    Setup leaderboard api and service layer

--- innocode/src/api/leaderboardApi.js
+++ innocode/src/api/leaderboardApi.js
@@ -1,11 +1,10 @@
 import axiosClient from "./axiosClient"
 
 const leaderboardApi = {
-  getAllByContestId: (contestId, { pageNumber = 1, pageSize = 10 } = {}) => {
+  getAllByContestId: (contestId, { pageNumber = 1, pageSize = 10 } = {}) =>
     axiosClient.get(`/leaderboard-entries/${contestId}`, {
       params: { pageNumber, pageSize },
-    })
-  },
+    }),
 }
 
 export default leaderboardApi
--- innocode/src/features/leaderboard/services/leaderboardService.js
+++ innocode/src/features/leaderboard/services/leaderboardService.js
@@ -1,3 +1,8 @@
+import leaderboardApi from "../../../api/leaderboardApi"
+
 export const leaderboardService = {
-    
+  async getAllByContestId(contestId, { pageNumber = 1, pageSize = 10 } = {}) {
+    const res = await leaderboardApi.getAllByContestId(contestId, { pageNumber, pageSize })
+    return res.data
+  },
 }
