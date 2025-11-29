56bd13a514c50c586d26761dad769af69bd29d6b
PeterNCTL <nguyencongthienloc10a3@gmail.com>
2025-11-29 21:55:11 +0700

    WIP judge submissions

--- innocode/src/api/testcaseApi.js
+++
@@ -1,7 +0,0 @@
-import axiosClient from "./axiosClient";
-
-export const testcaseApi = {
-  getRoundTestcases: (roundId) =>
-    axiosClient.get(`rounds/${roundId}/test-cases`),
-};
-export default testcaseApi;
